95ed7535ba00215d6bec987a84950082837f0bad
Magnus009 <fredrickrollamas2@gmail.com>
2021-04-17 22:50:55 +0800

    Backup & Restore

---
+++ 3KPet_MIS/Forms/frmBackupRestore.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBackupRestore
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblBackup = New System.Windows.Forms.Label()
+        Me.lblBUBorder = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.colDatabase = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colRestore = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Panel2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Purple
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(28, 272)
+        Me.Panel1.TabIndex = 7
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.DataGridView1)
+        Me.Panel2.Controls.Add(Me.btnBackup)
+        Me.Panel2.Controls.Add(Me.txtPath)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.lblBackup)
+        Me.Panel2.Controls.Add(Me.lblBUBorder)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(28, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(340, 272)
+        Me.Panel2.TabIndex = 8
+        '
+        'lblBackup
+        '
+        Me.lblBackup.AutoSize = True
+        Me.lblBackup.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBackup.Location = New System.Drawing.Point(19, 9)
+        Me.lblBackup.Name = "lblBackup"
+        Me.lblBackup.Size = New System.Drawing.Size(64, 19)
+        Me.lblBackup.TabIndex = 0
+        Me.lblBackup.Text = "BACKUP"
+        '
+        'lblBUBorder
+        '
+        Me.lblBUBorder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBUBorder.Location = New System.Drawing.Point(10, 20)
+        Me.lblBUBorder.Name = "lblBUBorder"
+        Me.lblBUBorder.Size = New System.Drawing.Size(318, 75)
+        Me.lblBUBorder.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(20, 101)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 19)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "RESTORE"
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Location = New System.Drawing.Point(11, 112)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(318, 142)
+        Me.Label2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 39)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "PATH :"
+        '
+        'txtPath
+        '
+        Me.txtPath.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPath.Location = New System.Drawing.Point(73, 35)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(244, 22)
+        Me.txtPath.TabIndex = 5
+        '
+        'btnBackup
+        '
+        Me.btnBackup.BackColor = System.Drawing.Color.Purple
+        Me.btnBackup.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btnBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBackup.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBackup.ForeColor = System.Drawing.Color.White
+        Me.btnBackup.Location = New System.Drawing.Point(170, 60)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(147, 25)
+        Me.btnBackup.TabIndex = 9
+        Me.btnBackup.Text = "&BACK UP DATABASE"
+        Me.btnBackup.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeColumns = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeight = 30
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colDatabase, Me.colRestore})
+        Me.DataGridView1.Location = New System.Drawing.Point(24, 123)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.Size = New System.Drawing.Size(293, 121)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'colDatabase
+        '
+        Me.colDatabase.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.colDatabase.HeaderText = "DATABASE BACKUP"
+        Me.colDatabase.Name = "colDatabase"
+        Me.colDatabase.ReadOnly = True
+        '
+        'colRestore
+        '
+        Me.colRestore.FillWeight = 25.0!
+        Me.colRestore.HeaderText = ""
+        Me.colRestore.Name = "colRestore"
+        Me.colRestore.ReadOnly = True
+        '
+        'frmBackupRestore
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(368, 272)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmBackupRestore"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "BACKUP & RESTORE"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblBackup As System.Windows.Forms.Label
+    Friend WithEvents lblBUBorder As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents colDatabase As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents colRestore As System.Windows.Forms.DataGridViewButtonColumn
+    Friend WithEvents btnBackup As System.Windows.Forms.Button
+End Class
---
+++ 3KPet_MIS/Forms/frmBackupRestore.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+
+Public Class frmBackupRestore
+
+    Private Sub frmBackupRestore_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        checkBackupDirectory()
+        txtPath.Text = "C:\BACKUP\Pet_MIS_" + Format(Today, "yyyyMMdd") + ".bak"
+    End Sub
+
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+        Try
+            sqlQuery = "BACKUP DATABASE Pet_MIS TO DISK = '" + txtPath.Text + "'"
+            If sqlExecute(sqlQuery) Then
+                MsgBox("Database Backup Successfully!", MsgBoxStyle.Information)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub checkBackupDirectory()
+        Dim strPath As String
+        strPath = "C:\BACKUP\"
+
+        If Not Directory.Exists(strPath) Then
+            Directory.CreateDirectory(strPath)
+        End If
+    End Sub
+End Class
